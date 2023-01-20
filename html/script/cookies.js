aec8ed56cbb15aa235f00695f13b5ff5c709ff12
hsc100zz@gmail.com <hsc100zz@gmail.com>
2023-01-20 22:13:06 +0100

    I swear it's the last one.

--- html/script/cookies.js
+++ html/script/cookies.js
@@ -1,11 +1,11 @@
 function user_login(username, password) {
-  document.cookie(user = username)
-  document.cookie(pass = password)
+  document.cookie = user = username
+  document.cookie = pass = password
 }
 function user_logout() {
-  document.cookie(user = null)
-  document.cookie(pass = null)
+  document.cookie = user = null
+  document.cookie = pass = null
 }
 function set_cust(name, value) {
-  document.cookie(name = value)
+  document.cookie = name = value
 }
