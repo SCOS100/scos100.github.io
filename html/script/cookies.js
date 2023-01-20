ec22124fd3c79997a70c4bab9343a47e10d49713
hsc100zz@gmail.com <hsc100zz@gmail.com>
2023-01-20 21:34:04 +0100

    Another...

---
+++ html/script/cookies.js
@@ -0,0 +1,11 @@
+function user_login(username, password) {
+  document.cookie(user = username)
+  document.cookie(pass = password)
+}
+function user_logout() {
+  document.cookie(user = null)
+  document.cookie(pass = null)
+}
+function set_cust(name, value) {
+  document.cookie(name = value)
+}
