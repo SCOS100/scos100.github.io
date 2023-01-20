2dbe295b4352ca0b35d3de499c28280791cb9e3c
hsc100zz@gmail.com <hsc100zz@gmail.com>
2023-01-20 20:08:02 +0100

    You know the deal.

--- html/script/firebase.js
+++ html/script/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-storage.js"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,51 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const storage = getStorage(app)
+function storageStart(namep) {
+   // Create the file metadata
+   /** @type {any} */
+   const metadata = {
+     contentType: 'image/png'
+   };
+   // Upload file and metadata to the object 'images/mountains.jpg'
+   const storageRef = ref(storage, 'images/' + namep);
+   const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+   // Listen for state changes, errors, and completion of the upload.
+   uploadTask.on('state_changed',
+     (snapshot) => {
+       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+       console.log('Upload is ' + progress + '% done');
+       switch (snapshot.state) {
+         case 'paused':
+           console.log('Upload is paused');
+           break;
+         case 'running':
+           console.log('Upload is running');
+           break;
+       }
+     }, 
+     (error) => {
+       // A full list of error codes is available at
+       // https://firebase.google.com/docs/storage/web/handle-errors
+       switch (error.code) {
+         case 'storage/unauthorized':
+           // User doesn't have permission to access the object
+           break;
+         case 'storage/canceled':
+           // User canceled the upload
+           break;
+         case 'storage/unknown':
+           // Unknown error occurred, inspect error.serverResponse
+           break;
+       }
+     }, 
+     () => {
+       // Upload completed successfully, now we can get the download URL
+       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+         console.log('File available at', downloadURL);
+       });
+     }
+   );
+} 
